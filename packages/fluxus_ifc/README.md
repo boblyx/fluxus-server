8485f7a41ffd6dd3914519987a2c39d412cfff92
boblyx <bobyxlee@gmail.com>
2024-06-01 19:28:40 +0800

    Exp: Used SpeckleClient to grab PropertySets via gql

---
+++ packages/fluxus_ifc/README.md
@@ -0,0 +1,5 @@
+# fluxus_ifc package
+- Basically transform everything in `ifctester.facet` from `IfcOpenShell` to work with individual `PropertySet`, `Entity`, `Attribute` etc `Facets` instead of using the whole IFC file. 
+
+## References
+- https://github.com/IfcOpenShell/IfcOpenShell/blob/v0.7.0/src/ifctester/ifctester/facet.py
--- test.py
+++ test.py
@@ -9,7 +9,23 @@ import ifctester.facet as Facet
 import ifcopenshell
 import os
 from pprint import pprint
+from specklepy.api.client import SpeckleClient
+from gql import gql
 
+TEST_QUERY = gql("""query Walls($query: [JSONObject!]){
+    stream(id: "90247e86c2"){
+        object(id: "b7d4aa78f723dea7e168b1a6bd2e09d3"){
+            children(query: $query select: ["type", "SGPset_WallStructuralLoad"]){
+                objects{
+                    data
+                }
+            }
+        }
+    }
+}
+""")
+
+SC = SpeckleClient(host = "app.speckle.systems");
 def executeIDS(ifc_path : str, ids_path : str):
     ifc = ifcopenshell.open(ifc_path)
     ids = ifctester.ids.open(ids_path)
@@ -18,7 +34,21 @@ def executeIDS(ifc_path : str, ids_path : str):
     pprint(report)
     return report
 
+def speckleGetObjPsets():
+    params = {
+            "query":{
+                "field": "type"
+                ,"value": "IFCWALL"
+                ,"operator": "="
+                }
+    }
+
+    response = SC.httpclient.execute(TEST_QUERY, params)
+    pprint(response)
+    return response["stream"]["object"]["children"]["objects"]
+
 if __name__ == "__main__":
+    """
     ifc_path = os.path.join(os.getcwd(), "examples", "example.ifc");
     ids_path = os.path.join(os.getcwd(), "examples", "IFC_SG_IDS_walls.xml");
     
@@ -32,7 +62,8 @@ if __name__ == "__main__":
     test_elem = elements[0]
     psets = Facet.get_psets(test_elem)
     pprint(psets);
-    #entity.filter(ifc, elements);
-    #pprint(elements);
     #executeIDS(ifc_path, ids_path);
+    """
+    objects = speckleGetObjPset();
+    # Given above Psets, validate them
     pass
