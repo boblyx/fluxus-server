e2f1f65c60ed34c66519387c394a0f159530bca2
Bob <bobyxlee@gmail.com>
2024-06-09 17:38:04 +0800

    feat: push updated object with new params

--- packages/API/Speckle.py
+++ packages/API/Speckle.py
@@ -6,6 +6,7 @@ Speckle.py
 from gql import gql
 from specklepy.api.client import SpeckleClient
 from pydantic import BaseModel
+from uuid import uuid4
 
 DEFAULT_SPECKLE_INFO = {"stream": "90247e86c2" 
                         ,"object": "b7d4aa78f723dea7e168b1a6bd2e09d3"
@@ -30,6 +31,68 @@ class QueryParam:
 
     def toDict(self):
         return {"query": self.__dict__}
+
+def getObjectData(client, stream, speckle_id):
+    query = """
+    query ObjQ{
+        stream(id:"%s"){
+            object(id:"%s"){
+                data
+            }
+        }
+    }
+    """ % (stream, speckle_id)
+    return client.httpclient.execute(gql(query))["stream"]["object"]["data"]
+
+def createObject(client, speckle_info, data):
+    mut = """
+    mutation ObjectCreate($objectInput: ObjectCreateInput!){
+        objectCreate(objectInput: $objectInput)
+    }
+    """
+    params = {
+        "objectInput": {
+                "streamId": speckle_info.stream,
+                "objects": [data]
+            }
+    }
+    obj = client.httpclient.execute(gql(mut), params)["objectCreate"]
+    return obj
+
+def createCommit(client, branch_name, speckle_info, obj_id):
+    mut = """
+    mutation CommitCreate($commit: CommitCreateInput!){
+        commitCreate(commit: $commit)
+    }
+    """
+    params = {
+        "commit": {
+                "streamId": speckle_info.stream,
+                "objectId": obj_id,
+                "branchName": branch_name,
+                "message": "Made by FamilyMan"
+            }
+        }
+    commit = client.httpclient.execute(gql(mut), params)["commitCreate"]
+    return commit
+
+def updateObjectParams(client : SpeckleClient 
+        ,update_data : dict
+        ,speckle_info : SpeckleInfo
+        ,branch_name : str):
+    # Get the original model
+    obj = getObjectData(client, speckle_info.stream, speckle_info.object)
+
+    # Apply the changes
+    for k, v in update_data.items():
+        obj[k] = v
+        pass
+    
+    obj["id"] = "fluxus-%s" % uuid4()
+    # Create a new object
+    obj_id = createObject(client, speckle_info, obj)[0];
+    commit_id = createCommit(client, branch_name, speckle_info, obj_id);
+    return "%s/projects/%s/models/%s" % (client.url, speckle_info.stream, obj_id)
 
 def cPsetQuery(pset_name : str, speckle_info : SpeckleInfo = DEFAULT_SPECKLE_INFO):
     """
